e3d180e1db7990e6ba2fa15f95c203e3187fcd3c
sumitsasidharan <s.sumit196@gmail.com>
2022-12-28 23:01:28 +0530

    finished buttons

--- src/components/MuiButton.tsx
+++ src/components/MuiButton.tsx
@@ -1,4 +1,4 @@
-import { Button, Stack } from '@mui/material';
+import { Button, Stack, IconButton, ButtonGroup } from '@mui/material';
 import AdbRoundedIcon from '@mui/icons-material/AdbRounded';
 import SendRoundedIcon from '@mui/icons-material/SendRounded';
 
@@ -46,16 +46,39 @@ export const MuiButton = () => {
             </Button>
          </Stack>
 
+         {/* Button with Icons and Text */}
          <Stack spacing={2} direction="row">
             <AdbRoundedIcon />
             <SendRoundedIcon />
 
-            <Button variant="contained" startIcon={<SendRoundedIcon />}>
+            <Button
+               variant="contained"
+               startIcon={<SendRoundedIcon />}
+               disableRipple
+               onClick={() => alert('hi sumit')}>
+               Alert
+            </Button>
+            <Button
+               variant="contained"
+               endIcon={<SendRoundedIcon />}
+               disableElevation>
                Send
             </Button>
-            <Button variant="contained" endIcon={<SendRoundedIcon />}>
-               Send
-            </Button>
+
+            {/* Icon button without any text */}
+            <IconButton aria-label="send" color="success" size="small">
+               <SendRoundedIcon />
+            </IconButton>
+         </Stack>
+
+         {/* Button Group */}
+         <h2>Button Group</h2>
+         <Stack direction="row">
+            <ButtonGroup variant="contained" size="small" orientation='vertical' color="secondary" >
+               <Button onClick={() => alert("Left clicked")} >Left</Button>
+               <Button>Center</Button>
+               <Button>Right</Button>
+            </ButtonGroup>
          </Stack>
       </Stack>
    );
